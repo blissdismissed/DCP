0e6d907f1ece7cbf0035010441229de6824e5b5f
Arthur <ajautz@gmail.com>
2019-09-25 19:44:19 -0400

    Add Sept 25

---
+++ Sept25.py
@@ -0,0 +1,10 @@
+#Sept 25#
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+if __name__ == "__main__":
+    print("Boom")
