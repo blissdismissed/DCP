505f523383b38b64b9b0a9d08820eaf0b38e142b
Arthur <ajautz@gmail.com>
2019-09-25 20:06:17 -0400

    Problem solved

--- Sept25.py
+++ Sept25.py
@@ -6,11 +6,33 @@ class Node:
         self.left = left
         self.right = right
 
+
+def serialize(node):
+    # init empty string
+    serialStr = []
+    looplst = [node.val, node.left, node.right]
+    for i in looplst:
+        serialStr.append(i)
+    return serialStr
+
+
+def deserialize(serialStr):
+    #for i in serialStr:
+    node = Node(serialStr[0],serialStr[1],serialStr[2])
+    return node
+
+
 if __name__ == "__main__":
 
-    node = Node('root')
-    print(node)
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+    #print(node.val)
+    #print(node.left.left.val)
+    #print(node.right.val)
+    #print(serialize(node))
+    #print(deserialize(serialize(node)).left.val)
+
+
 
 
     #node = Node('root', Node('left', Node('left.left')), Node('right'))
-    #assert deserialize(serialize(node)).left.left.val == 'left.left'    
+    assert deserialize(serialize(node)).left.left.val == 'left.left'    
